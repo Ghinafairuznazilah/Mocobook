215c64aa02c5052805551318fef2bbeda7f716c4
Ghinafairuznazilah <ghina.fairuz.nazilah@gmail.com>
2021-10-28 22:52:40 +0700

    logic display detail books done

--- Homepage/list.js
+++ Homepage/list.js
@@ -2,11 +2,11 @@ function showCards(m){
     return `
     <div class="col-md-4 my-3">
                 <div class="card">
-                    <img src="" class="card-img-top" alt="">
+                    <img src="${m.volumeInfo.imageLinks.thumbnail}" class="card-img-top" alt="">
                     <div class="card-body">
                       <h5 class="card-title">${m.volumeInfo.title}</h5>
-                      <h6 class="card-subtitle mb-2 text-muted">${m.auhtor}</h6>
-                      <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#booksdetailModal">
+                      <h6 class="card-subtitle mb-2 text-muted">${m.volumeInfo.authors}</h6>
+                      <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#booksdetailModal" data-imdbid="${m.id}">
                        Show Details</a>
                     </div>
                   </div>
@@ -20,15 +20,16 @@ function showBooksDetail(m){
     <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
-            <img src="${m.img}" class="img-fluid">
+            <img src="${m.volumeInfo.imageLinks.thumbnail}" class="img-fluid">
         </div>
         <div class="col-md">
           <ul class="list-group">
-              <li class="list-group-item"><h4>judul buku</h4></li>
-              <li class="list-group-item"><strong>penulis : </strong> Ahemsa Azaleav</li>
-              <li class="list-group-item"><strong>penulis : </strong> Ahemsa Azaleav</li>
-              <li class="list-group-item"><strong>penulis : </strong> Ahemsa Azaleav</li>
-              <li class="list-group-item"><strong>penulis : </strong> <br> Ahemsa Azaleav</li>
+              <li class="list-group-item"><strong>Judul Buku : </strong> ${m.volumeInfo.title}</li>
+              <li class="list-group-item"><strong>Sub Judul : </strong> ${m.volumeInfo.subtitle}</li>
+              <li class="list-group-item"><strong>Penulis    : </strong> ${m.volumeInfo.authors}</li>
+              <li class="list-group-item"><strong>publisher  : </strong> ${m.volumeInfo.publisher}</li>
+              <li class="list-group-item"><strong>Jumlah Hal : </strong> ${m.volumeInfo.pageCount}</li>
+              <li class="list-group-item"><strong>Deskripsi : </strong> <br> ${m.volumeInfo.description} </li>
             </ul>
         </div>
     </div>
@@ -48,6 +49,26 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=' + inputKeyword.value)
         const booksContainer = document.querySelector('.books-container')
         booksContainer.innerHTML = cards;
         
+        // ketika button modal di klik
+        const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+        modalDetailButton.forEach(btn => {
+            btn.addEventListener('click', function () {
+            const imdbid = this.dataset.imdbid;
+                fetch('https://www.googleapis.com/books/v1/volumes?q='+imdbid)
+                .then(response=> response.json())
+                .then(m => {
+                    // const booksDetail = showBooksDetail(m);
+                    // const modalBody = document.querySelector('.modal-body');
+                    // modalBody.innerHTML = booksDetail;
+                    const detail = response.items;
+                    let booksDetail = ""
+                    detail.forEach(m => booksDetail = showBooksDetail(m));
+                    const modalBody = document.querySelector('.modal-body');
+                    modalBody.innerHTML = booksDetail;
+
+                }) ;
+            });
+        })
     }
 );
 });
